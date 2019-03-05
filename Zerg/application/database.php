db79d75d9dbb13fa2a5b0e8ffc10afa4dbcfe03c
dzz <2448676749@qq.com>
2019-03-05 20:30:24 +0800

    修改thinkphp数据库配置

--- Zerg/application/database.php
+++ Zerg/application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '101.200.236.82',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'terran',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '19851118',
+    'password'        => 'qq89347399',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
